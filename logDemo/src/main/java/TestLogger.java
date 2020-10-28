833aaddd0d21f3541c64a448a5f297d5aebd84f2
wangluyan <wangluyan128>
2020-10-28 17:27:21 +0800

    日志输出demo

---
+++ logDemo/src/main/java/TestLogger.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class TestLogger {
+
+    public static void main(String[] args) throws IOException {
+    /*    Logger log = Logger.getLogger("lavasoft");
+        log.setLevel(Level.INFO);
+        Logger log1 = Logger.getLogger("lavasoft");
+        System.out.println(log==log1);
+        Logger log2 = Logger.getLogger("lavasoft.blog");
+       // log2.setLevel(Level.WARNING);
+
+        //控制台控制器
+        ConsoleHandler consoleHandler = new ConsoleHandler();
+        consoleHandler.setLevel(Level.ALL);
+        log.addHandler(consoleHandler);
+        //文件控制器
+        FileHandler fileHandler = new FileHandler("/logs/1.log");
+        fileHandler.setLevel(Level.INFO);
+        log.addHandler(fileHandler);
+
+        log.info("aaa");
+        log2.info("bbb");
+        log2.fine("fine");*/
+        String test = "sdibt";
+
+    }
+}
