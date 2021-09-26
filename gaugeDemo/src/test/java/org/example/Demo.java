ec19a6ae624200e459161fdc9a42fbc8fd039544
wangyan2 <wangyan2@mchz.com.cn>
2021-09-26 10:18:50 +0800

    增加 demo用例

---
+++ gaugeDemo/src/test/java/org/example/Demo.java
@@ -0,0 +1,56 @@
+package org.example;
+
+import com.thoughtworks.gauge.Step;
+
+public class Demo {
+
+    @Step("开始看电视")
+    public void implementation1() {
+
+    }
+
+    @Step("主角<韦小宝>降临")
+    public void implementation2(Object arg0) {
+
+    }
+
+    @Step("他乡遇故知<陈近南>,成功他的徒弟")
+    public void implementation3(Object arg0) {
+
+    }
+
+    @Step("主角 <韦小宝> 降临")
+    public void implementation4(Object arg0) {
+
+    }
+
+    @Step("联合 <乾隆> 打败 <鳌拜>")
+    public void implementation5(Object arg0, Object arg1) {
+
+    }
+
+    @Step("认识许多漂亮女孩 <table>")
+    public void implementation6(Object arg0) {
+
+    }
+
+    @Step("认识许多其他的漂亮女孩<table:gril.csv>")
+    public void implementation7(Object arg0) {
+
+    }
+
+    @Step("关机睡觉")
+    public void implementation8() {
+
+    }
+
+    @Step("遇到漂亮的<gril>")
+    public void implementation9(Object arg0) {
+
+    }
+
+    @Step("追求到她，并同她结婚")
+    public void implementation10() {
+
+    }
+}
